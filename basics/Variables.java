07f951487e773600a0598b32cf2f2379caea98d7
Pratheesha S <134820817+Pratheeshasanil@users.noreply.github.com>
2026-02-03 22:49:50 -0800

    Add Variable class with main method

---
+++ basics/Variables.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String args[]){
+        int container = 67;
+        String Puppy = "S";
+        String container2 = "Puppy";
+        System.out.println(container);
+        System.out.println(container2);
+    
+}
+
+    }
+        
