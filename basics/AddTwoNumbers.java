aaa8c6644bb0699c201ceb9d7264730e121d9716
Pratheesha S <134820817+Pratheeshasanil@users.noreply.github.com>
2026-02-04 21:01:53 -0800

    Create AddTwoNumbers.java

---
+++ basics/AddTwoNumbers.java
@@ -0,0 +1,16 @@
+// Java Console Program – Add Two Numbers
+// Practice project for learning input/output in Java
+
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner pup = new Scanner(System.in);
+
+        int a = pup.nextInt();
+        int b = pup.nextInt();
+
+        int c = a + b;
+        System.out.println(c);
+    }
+}
